a20dba7b8bf1ec035250539890f3df291bbbf9a1
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-07 18:12:38 +0100

    correction to lenght

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@ var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na 
 var animal ='Zielone słonie'
 var animalUpperCased = animal.toUpperCase();
 var textAfter = text.replace('Papugi', animalUpperCased);
-var textAfterRedone = textAfter.substr(0,length/2)
+var textAfterRedone = textAfter.substr(0 ,textAfter.length/2)
 console.log(textAfterRedone)
