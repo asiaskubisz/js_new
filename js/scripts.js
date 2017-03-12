bea2c52b2cf99bab49919717117e0897e4b54cca
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-12 20:43:11 +0100

    all

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
-var newName ='Marian'
+$(function(){
+	//ten kod wykona się po załadowaniu DOM
+});
 
-if (allNames.indexOf(newName) === -1) {
-    var x = allNames.push(newName);
+var carouselList = $("#carousel ul");
+
+setInterval(changeSlide, 3000);
+
+function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 }
-console.log(allNames);
+
+function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");  
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+}
