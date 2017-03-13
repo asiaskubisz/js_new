8e67493dc077d40369c6f1d386305fa9ba55649a
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-13 11:42:38 +0100

    jkd

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(function(){
-	//ten kod wykona się po załadowaniu DOM
+    
+
+	var carouselList = $("#carousel ul");
+
+        function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");  
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+}
+      function changeSlide() {
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+}
+    
+    setInterval(changeSlide, 3000);
+
+  
+
+
 });
 
-var carouselList = $("#carousel ul");
-
-setInterval(changeSlide, 3000);
-
-function changeSlide() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-}
-
-function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");  
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-}
