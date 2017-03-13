9c9d0516caa9f824538db0af1ceb097a000237cc
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-13 11:48:10 +0100

    je

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $(function(){
     
-
 	var carouselList = $("#carousel ul");
 
-        function moveFirstSlide() {
+    function moveFirstSlide() {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");  
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
 }
-      function changeSlide() {
+    function changeSlide() {
         carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 }
     
