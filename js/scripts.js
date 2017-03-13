145ea9a685a59691bd10fc937bd7177b0470f549
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-13 11:32:32 +0100

    je

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var buttonNamesByClass = document.getElementsByClassName('button');
 console.log(buttonNamesByClass);
 for (k = 0; k < buttonNamesByClass.length; k++) {
-var tab = document.getElementsByClassName("button")[k].textContent;
-}
+    var tab = document.getElementsByClassName("button")[k].textContent;
 alert(tab)
+}
