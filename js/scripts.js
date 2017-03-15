7328e55f929f08d43c2c9b0b6c4ea87e069348f4
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-15 13:11:37 +0100

    corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 function Telefon(marka, cena, kolor) {
     this.marka = marka;
-        this.cena = cena;
-        this.kolor = kolor;
+    this.cena = cena;
+    this.kolor = kolor;
 }
 
-Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka +", kolor to " + this.kolor +", a cena to " + this.cena +".");
+Telefon.prototype.printInfo = function(){
+	console.log("Marka telefonu to " + this.marka +", kolor to " + this.kolor +", a cena to " + this.cena +".")}
+    
+var iPhone7 = new Telefon("Apple", 2250, "srebrny");
+var SamsungGalaxyS6 = new Telefon("Samsung", 1300, "czarny");
+var OnePlusOne = new Telefo("ChineseMaker", 400, "zielony");
+    
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
