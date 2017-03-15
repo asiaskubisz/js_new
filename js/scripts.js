a4c2e414ce01faddfe186562103ca14dac2535c6
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-15 19:39:28 +0100

    jdl

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
-var newName ='Marian'
+$(function(){
+    function Button(text) {
+        this.text = "text" || 'Hello';
+    }
+    Button.prototype = {
+        create: function() {
+	    var self = this;
+	    this.$element = $(‘<button>’);
+	    this.$element.text(this.text);
+	    this.$element.click(function() {
+		  alert(self.text);
+	      });
+        $(‘body’).append(this.$element);
+        }
+    }
 
-if (allNames.indexOf(newName) === -1) {
-    var x = allNames.push(newName);
+    var btn1 = new Button(‘Hello!’);
+    btn1.create();
 }
-console.log(allNames);
+  
+
+
