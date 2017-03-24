f0bc1cd9310820840ad760e7ed37bdbb7d946ccc
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-24 11:18:32 +0100

    kd

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
-var newName ='Marian'
+$(function() {
 
-if (allNames.indexOf(newName) === -1) {
-    var x = allNames.push(newName);
+    var url = 'http://api.icndb.com/jokes/random';
+
+    var button = document.getElementById('get-joke');
+    button.addEventListener('click', function() {
+        getJoke();
+    });
+
+    var paragraph = document.getElementById('joke');
+
+    function getJoke() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', function() {
+            var response = JSON.parse(xhr.response);
+            paragraph.innerHTML = response.value.joke;
+        });
+        xhr.send();
+    }
+}); // This is just a sample script. Paste your real code (javascript or HTML) here.
+
+if ('this_is' == /an_example/) {
+    of_beautifier();
+} else {
+    var a = b ? (c % d) : e[f];
 }
-console.log(allNames);
