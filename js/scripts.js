bfc84ab2779df78c14fdb57c39ef90a034c1b96c
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-24 11:21:25 +0100

    kjd

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,4 @@ $(function() {
         });
         xhr.send();
     }
-}); // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
-}
+});
