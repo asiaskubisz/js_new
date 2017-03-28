de9a6c603692a241f6b8ce378842b039dd1f8a35
Joanna Skubisz <joannaskubisz85@gmail.com>
2017-03-28 14:42:17 +0200

    jk

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
-var newName ='Marian'
+$(function() {
+            var url = 'https://restcountries.eu/rest/v1/name/';
+            var countriesList = $('#countries');
 
-if (allNames.indexOf(newName) === -1) {
-    var x = allNames.push(newName);
-}
-console.log(allNames);
+            $('#search').click(searchCountries);
+
+            function searchCountries() {
+                var countryName = $('#country-name').val();
+                if (!countryName.length) countryName = 'Poland';
+                $.ajax({
+                    url: url + countryName,
+                    method: 'GET',
+                    success: showCountriesList
+                });
+            }
+
+            function showCountriesList(resp) {
+                countriesList.empty();
+                resp.forEach(function(item) {
+                    resp.forEach(function(item) {
+                        $('<li>').text(item.name).appendTo(countriesList);
+                    });
+                });
+            }
+
+        }
